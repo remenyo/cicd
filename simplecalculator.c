8355b99cc4282967ace8c887d3415640c976e46e
remenyo <remenyo@gmail.com>
2024-03-13 10:46:16 +0100

    Fixed scanf (void) but made opaddition bad

--- simplecalculator.c
+++ simplecalculator.c
@@ -62,7 +62,7 @@ char getoperation()
            "[m] Multiplication\n"
            "[d] Division\n"
            ":");
-    scanf(" %c", &choosen);
+    (void)scanf(" %c", &choosen);
     return choosen;
 }
 
@@ -70,6 +70,6 @@ int getoperand(const char *which)
 {
     int operand = 0;
     printf("Operand %s: ", which);
-    scanf(" %d", &operand);
+    (void)scanf(" %d", &operand);
     return operand;
 }
